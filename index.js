0acd9c611926edcc91f78e39819bacf07d145b39
mahfuz swaron <mahfuzswaron@gmail.com>
2022-12-30 09:07:49 -0500

    game logics done

--- index.js
+++ index.js
@@ -17,6 +17,41 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 const usersCollection = client.db("tic-tac-toe").collection("users");
 const gamesCollection = client.db("tic-tac-toe").collection("games");
 
+const gameResult = (board, pieces) => {
+    const keySets = [
+        ["a1", "a2", "a3"],
+        ["b1", "b2", "b3"],
+        ["c1", "c2", "c3"],
+        ["a1", "b1", "c1"],
+        ["a2", "b2", "c2"],
+        ["c1", "c2", "c3"],
+        ["a1", "b1", "c1"],
+        ["a2", "b2", "c3"],
+        ["a3", "b3", "c3"],
+        ["a1", "b2", "c3"],
+        ["a3", "b2", "c1"]
+    ];
+    let winnerPiece = null;
+    for (let i = 0; i < keySets.length; i++) {
+        const set = keySets[i];
+        let matchedCount = 0;
+        set.forEach((x) => board[set[0]] === board[x] && matchedCount++)
+        if (matchedCount === 3) winnerPiece = board[set[0]]
+        // console.log(mathcedCount)
+    }
+    // console.log(winnerPiece);
+    if (winnerPiece) {
+        return pieces[winnerPiece]
+    }
+    else {
+        return null
+    }
+
+};
+
+const alternateMove = (players, lastMove) => Object.values(players).find(p => p !== lastMove);
+
+
 const run = async () => {
     try {
         client.connect();
@@ -80,7 +115,7 @@ const run = async () => {
                 },
                 status: {
                     finished: false,
-                    message: `${move}'s Move"`
+                    message: `${move}'s Move`
                 },
                 lastUpdated: new Date()
             };
@@ -97,16 +132,60 @@ const run = async () => {
             const user = req.params.user;
             const query = { $or: [{ "players.initializer": user }, { "players.partner": user }] }
             const games = await gamesCollection.find(query).toArray();
-            console.log(games)
+            // console.log(games)
             res.send(games);
         });
 
         // get single game 
+
+        const getGameById = async id => {
+            const game = await gamesCollection.findOne({ _id: ObjectId(id) })
+            return game;
+        };
+
         app.get("/get-game/:id", async (req, res) => {
+            const id = req.params.id
+            const game = await getGameById(id);
+            res.send(game);
+        });
+
+        /*
+        game logic:
+        if the board is completed: check who wins or draw
+        else: update current board
+
+        */
+
+        // update a game 
+        app.patch("/play/:id", async (req, res) => {
             const id = ObjectId(req.params.id);
             const query = { _id: id };
-            const game = await gamesCollection.findOne(query);
-            res.send(game);
+            const board = req.body;
+            const game = getGameById(id);
+            const move = alternateMove(game.players, game.move);
+            const updatedGame = {
+                board: board,
+                move: move,
+                status: {
+                    finished: false,
+                    message: `${game.move} has moved.\nNow it's ${move}'s move`
+                },
+                lastUpdated: new Date()
+            }
+            if (!Object.values(board).filter(x => x === "").length) { // checks if any move available
+                let message;
+                const winner = gameResult(board, game.pieces);
+                if (winner) {
+                    message = `${winner} has won the match`
+                }
+                else if (!winner) {
+                    message = "This game is draw. You both won!"
+                }
+                updatedGame.status = { finished: true, message: message }
+            }
+
+
+
         })
 
     }
